aca22498ec55580ad68f182da502a3b76ba7b6d9
bonheurdev <125626137+bonheurdev@users.noreply.github.com>
2023-06-05 21:10:25 +0200

    Update 8-concat_edges.py

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str[39:66] + " " + str[107:111] + " " + str[:6]
+str = str[39:66] + str[106:112] + str[:6]  # on second string include spaces
 print(str)
