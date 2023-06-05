5b6a4e97505d690d3a618d65b0fc14852180512f
bonheurdev <125626137+bonheurdev@users.noreply.github.com>
2023-06-06 00:41:49 +0200

    Update 102-magic_calculation.py

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 # it is implemented it as follows
 def magic_calculation(a, b):
-    return (98 + a ** b)  # it implements all
+    result = 98  # LOAD_CONST 1 (98)
+    result += a  # LOAD_FAST 0 (a)
+    result **= b  # LOAD_FAST 1 (b) + BINARY_POWER
+    return result  # RETURN_VALUE
