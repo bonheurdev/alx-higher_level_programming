6eafe08f873fba69185bdb1382ba1c513fd46794
bonheurdev <125626137+bonheurdev@users.noreply.github.com>
2023-06-05 22:40:28 +0200

    Update 100-write.py

---
+++ 0x00-python-hello_world/100-write.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+import sys
+# Writing to standard error
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+# for it is error exiting with status code 1
+sys.exit(1)
