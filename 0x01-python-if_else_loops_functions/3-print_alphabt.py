7547b4440b479e47d121709842613bf941cbcc3d
bonheurdev <125626137+bonheurdev@users.noreply.github.com>
2023-06-06 23:19:56 +0200

    Update 3-print_alphabt.py

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Print all the letters except q and e"""
+start = ord('a')
+end = ord('z') + 1
+for i in range(start, end):
+    char = chr(i)
+    if char != 'e' and char != 'q':
+        print("{}".format(char), end="")
