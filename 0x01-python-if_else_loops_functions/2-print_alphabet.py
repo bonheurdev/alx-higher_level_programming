025ec9b6fdddc1c5ee41ee9487e3a59d18b3d82f
bonheurdev <125626137+bonheurdev@users.noreply.github.com>
2023-06-06 23:03:49 +0200

    Update 2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-# the ord() function is used to get the ASCII value of a character
+"""Print the alphabet in lowercase"""
 start = ord('a')
 end = ord('z') + 1
 for i in range(start, end):
-    char = chr(i)  # convert an ASCII value back to character
-    print(char, end="")
+    # convert an ASCII value back to character
+    print("{}".format(chr(i)), end="")
