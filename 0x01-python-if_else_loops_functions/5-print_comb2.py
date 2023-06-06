a83727f7a7283b72c6a04b9231887b5068c2daa6
bonheurdev <125626137+bonheurdev@users.noreply.github.com>
2023-06-07 00:17:15 +0200

    Update prints numbers from 0 to 99

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" prints numbers from 0 to 99 in ascending order, with two digits"""
+for i in range(100):
+    if i == 99:  # Check if it's the last number
+        print("{}".format(i))
+    else:
+        print("{:02}".format(i), end=", ")
