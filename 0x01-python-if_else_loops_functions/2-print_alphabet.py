6b1f031d55b9fa41eb16ce92b1c1a9ae0886a10d
bonheurdev <125626137+bonheurdev@users.noreply.github.com>
2023-06-06 22:55:15 +0200

    Update 2-print_alphabet.py

---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# the ord() function is used to get the ASCII value of a character
+start = ord('a')
+end = ord('z') + 1
+for i in range(start, end):
+    char = chr(i)  # convert an ASCII value back to character
+    print(char, end="")
