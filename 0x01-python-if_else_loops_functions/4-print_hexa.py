40000a15f2ccc2be366a9bd8bfa105426629d16f
bonheurdev <125626137+bonheurdev@users.noreply.github.com>
2023-06-06 23:45:40 +0200

    Update 4-print_hexa.py

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""prints all numbers from 0 to 98 in decimal and in hexadecimal"""
+"""prints all numbers from 0 to 99 in decimal and in hexadecimal"""
 for i in range(99):
     var = '0x'  # variable to store 0x
     print('{} = {}{:x}'.format(i, var, i))
