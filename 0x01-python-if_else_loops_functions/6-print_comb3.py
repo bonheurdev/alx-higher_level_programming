4130c7d64754c7fd52e0c89bd8d1ba8350d8827d
bonheurdev <125626137+bonheurdev@users.noreply.github.com>
2023-06-07 10:23:25 +0200

    all possible different combinations of two digits 100 numbers

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+for i in range(10):
+    for j in range(10):
+        # combine the values of i and j into a two-digit number
+        var1 = i * 10 + j
+        if var1 == 89:
+            print('{:02}'.format(var1))
+        elif j > i:
+            print('{:02}'.format(var1), end=", ")
