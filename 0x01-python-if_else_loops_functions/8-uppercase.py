6ce7cb0a1961bef58099d4dc0605426adf66db85
bonheurdev <125626137+bonheurdev@users.noreply.github.com>
2023-06-07 15:59:34 +0200

    Update 8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""function that prints a string in uppercase"""
+
+
+def uppercase(str):
+    for char in str:
+        var1 = ord(char)
+        if (var1 >= 97) and (var1 <= 122):      
+            var2 = chr(var1 - 32)
+        else:
+            var2 = chr(var1)
+        print(var2, end="")
+    print()
