046d4a1b003e1d506b441800c16cb38ea12bd14b
bonheurdev <125626137+bonheurdev@users.noreply.github.com>
2023-06-07 19:56:19 +0200

    alternating lowercase and uppercase in reverse

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+start = ord('a') - 1
+end = ord('z')
+
+for charac in range(end, start, -2):
+    var1 = chr(charac) + chr((charac - 32) - 1)
+    print(var1, end="")
+print("")
