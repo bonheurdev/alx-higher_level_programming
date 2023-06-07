b3a4dee05e7ad6ef0b769633b0fb6d4eab263b70
bonheurdev <125626137+bonheurdev@users.noreply.github.com>
2023-06-07 22:28:15 +0200

    function for bytecode

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -3,7 +3,7 @@
   3           0 LOAD_FAST                0 (a)
               3 LOAD_FAST                1 (b)
               6 COMPARE_OP               0 (<)
-              9 POP_JUMP_IF_FALSE       16    
+              9 POP_JUMP_IF_FALSE       16
 
   4          12 LOAD_FAST                2 (c)
              15 RETURN_VALUE
@@ -11,7 +11,7 @@
   5     >>   16 LOAD_FAST                2 (c)
              19 LOAD_FAST                1 (b)
              22 COMPARE_OP               4 (>)
-             25 POP_JUMP_IF_FALSE       36    
+             25 POP_JUMP_IF_FALSE       36
 
   6          28 LOAD_FAST                0 (a)
              31 LOAD_FAST                1 (b)
@@ -30,7 +30,6 @@
 def magic_calculation(a, b, c):
     if a < b:
         return c
-    elif c > b:
+    if c > b:
         return a + b
-    else:
-        return a * b - c
+    return a * b - c
