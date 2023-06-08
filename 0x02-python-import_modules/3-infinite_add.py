0b20660e38516f8c9cf9e64ab67de159b3224cd4
bonheurdev <125626137+bonheurdev@users.noreply.github.com>
2023-06-08 19:12:03 +0200

    prints the result of the addition of all arguments

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+
+    import sys
+    m = len(sys.argv)
+    add = 0
+    for i in range(1, m):
+        add += int(sys.argv[i])
+    print("{}".format(add))
