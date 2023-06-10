e9935bb3b6001c8696dfe795ec136c19443c8008
bonheurdev <125626137+bonheurdev@users.noreply.github.com>
2023-06-10 22:13:36 +0200

    retrieves an element from a list like in C

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" retrieves an element from a list like in C"""
+
+
+def element_at(my_list, idx):
+    length = len(my_list)
+    if idx < 0:
+        return None
+    elif idx >= length:
+        return None
+    else:
+        return my_list[idx]
