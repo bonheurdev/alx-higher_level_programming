30fc99f54f8e17979e199ad9ff7a8854d1db9fb1
bonheurdev <125626137+bonheurdev@users.noreply.github.com>
2023-06-14 16:02:13 +0200

    computes the square value of all integers of a matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""computes the square value of all integers of a matrix"""
+
+
+def square_matrix_simple(matrix=[]):
+    var_matrix = [[second ** 2 for second in first] for first in matrix]
+    return var_matrix
