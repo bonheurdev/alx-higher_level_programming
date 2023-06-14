41c8b8cc924d49755d711e30e9013863e6de159b
bonheurdev <125626137+bonheurdev@users.noreply.github.com>
2023-06-14 21:07:50 +0200

    returns a set of all elements present in only one set

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""returns a set of all elements present in only one set"""
+
+
+def only_diff_elements(set_1, set_2):
+    diff = []
+    for x in set_1:
+        if x not in set_2:
+            diff.append(x)
+    for y in set_2:
+        if y not in set_1:
+            diff.append(y)
+    return diff
