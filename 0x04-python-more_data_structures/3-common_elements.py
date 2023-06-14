30255e4634ea2566f61fb8394b5d536b38f82b1e
bonheurdev <125626137+bonheurdev@users.noreply.github.com>
2023-06-14 19:59:00 +0200

    returns a set of common elements in two sets

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" a function that returns a set of common elements in two sets"""
+
+
+def common_elements(set_1, set_2):
+    co_element = list(filter(lambda x: x in set_1 and x in set_2, set_1))
+    return co_element
