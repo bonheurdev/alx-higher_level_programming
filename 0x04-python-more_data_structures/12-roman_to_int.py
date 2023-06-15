cc946550dd77c0bc0d42897574054750fc2fb3df
bonheurdev <125626137+bonheurdev@users.noreply.github.com>
2023-06-15 11:48:00 +0200

    Update 12-roman_to_int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,31 +5,26 @@
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
-    Roman_Units = {
+
+    roman_numerals = {
         'I': 1, 'II': 2, 'III': 3, 'IV': 4, 'V': 5,
-        'VI': 6, 'VII': 7, 'VIII': 8, 'IX': 9
-    }
-    Roman_Tens = {
+        'VI': 6, 'VII': 7, 'VIII': 8, 'IX': 9,
         'X': 10, 'XX': 20, 'XXX': 30, 'XL': 40, 'L': 50,
-        'LX': 60, 'LXX': 70, 'LXXX': 80, 'XC': 90
-    }
-    Roman_Hundr = {
+        'LX': 60, 'LXX': 70, 'LXXX': 80, 'XC': 90,
         'C': 100, 'CC': 200, 'CCC': 300, 'CD': 400, 'D': 500,
-        'DC': 600, 'DCC': 700, 'DCCC': 800, 'CM': 900
-    }
-    Roman_Thous = {
+        'DC': 600, 'DCC': 700, 'DCCC': 800, 'CM': 900,
         'M': 1000, 'MM': 2000, 'MMM': 3000
     }
+
     result = 0
 
-    for s in roman_string:
-        add_result = 0
-        string = []
-        string.append(s)
-        for dictionary in [Roman_Units, Roman_Tens, Roman_Hundr, Roman_Thous]:
-            if "".join(string) in dictionary:
-                add_result = dictionary["".join(string)]
+    while roman_string:
+        for numeral in sorted(roman_numerals, key=len, reverse=True):
+            if roman_string.startswith(numeral):
+                result += roman_numerals[numeral]
+                roman_string = roman_string[len(numeral):]
                 break
-        result += add_result
+        else:
+            return 0
 
     return result
