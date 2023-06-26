85047ddc07b2b0212d6fcf098938dd7c3d6e4721
bonheurdev <125626137+bonheurdev@users.noreply.github.com>
2023-06-27 01:34:39 +0200

    function for bytecode

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""the python function that is writen according to bytecode"""
+
+
+def magic_calculation(a, b):
+    result = 0
+
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception('Too far')
+            result += a ** b / i
+        except Exception:
+            result += a + b
+
+    return result
