d9b978a332ef4417f40f5ca2d371dc5241186658
bonheurdev <125626137+bonheurdev@users.noreply.github.com>
2023-06-27 01:15:22 +0200

    a function that executes a function safely

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+"""a function that executes a function
+and handle exceptions safely
+"""
+
+
+def safe_function(fct, *args):
+    try:
+        result = fct(*args)
+        return result
+    except Exception as e:
+        print(f"Exception: {e}", file=sys.stderr)
+        return None
