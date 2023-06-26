932faa34ee1d9c858e1aea36ef6230fe478c7de0
bonheurdev <125626137+bonheurdev@users.noreply.github.com>
2023-06-26 22:47:50 +0200

    prints the first x elements of a list with exceptions handled in silence

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""a function that prints the first x elements of a list and only integers"""
+
+
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+
+    for index in range(x):
+        try:
+            print("{:d}".format(my_list[index]), end="")
+            count += 1
+        except (TypeError, ValueError):
+            pass
+    print()  # print new line
+    return count
