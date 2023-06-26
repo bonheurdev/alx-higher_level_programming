36dbfcbb59149c36d69a6e80a34bb366f263443a
bonheurdev <125626137+bonheurdev@users.noreply.github.com>
2023-06-26 21:06:51 +0200

    a function that prints x elements of a list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""error handling using try and except in the safe_print_list function"""
+
+
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    try:
+        for index in range(x):
+            print(my_list[index], end='')
+            count += 1
+    except IndexError:
+        pass
+
+    print()
+    return count
