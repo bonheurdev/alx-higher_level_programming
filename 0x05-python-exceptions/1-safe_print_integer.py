929e7395dc31ed27838aae049cf7abc8a60a8570
bonheurdev <125626137+bonheurdev@users.noreply.github.com>
2023-06-26 21:31:25 +0200

    prints an integer and handle exceptions

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""a function that  prints an integer and handle exceptions"""
+
+
+def safe_print_integer(value):
+    try:
+        int_value = int(value)
+        print("{:d}".format(int_value))
+        return True
+    except ValueError:
+        return False
