7c97266b30969619621ae8b0cc5181c1051b66ea
bonheurdev <125626137+bonheurdev@users.noreply.github.com>
2023-06-27 16:38:38 +0200

    an empty class Square that contains Private instance attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""creating an empty class Square that defines a square"""
+
+
+class Square:
+    """an empty class called square."""
+    def __init__(self, size):
+        """ initializing function
+        Args: int: size of square
+        """
+        self.__size = size
