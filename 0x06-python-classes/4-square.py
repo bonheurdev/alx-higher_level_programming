c07d22a2e87eac22908ca7a69d65b3c50d4a3b5e
bonheurdev <125626137+bonheurdev@users.noreply.github.com>
2023-06-27 17:59:26 +0200

    Access and update private attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""creating an empty class Square that defines a square"""
+
+
+class Square:
+    """An empty class called square."""
+
+    def __init__(self, size=0):
+        """Initializing function
+        Args: int(size): size of square
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """function to retrive size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """function to set proper value of size"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Returns the current square area"""
+        return self.__size * self.__size
