9eab6d1d2c52b5d2690d0b89a16c9b949ad90892
bonheurdev <125626137+bonheurdev@users.noreply.github.com>
2023-07-05 17:17:47 +0200

    String representation

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -50,7 +50,7 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return (0)
         return ((self.__width * 2) + (self.__height * 2))
-        
+
     def __str__(self):
         """method to return a string representation of the object
         with character #
