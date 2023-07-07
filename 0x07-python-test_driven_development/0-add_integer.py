52eb318e98b1a6bd24b1d8b06fc96224561b37f6
bonheurdev <125626137+bonheurdev@users.noreply.github.com>
2023-07-07 21:47:59 +0200

    Integers addition

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""adds a and b as integers.
+
+    Raises:
+        TypeError: If either of a or b is a non-integer and non-float.
+    """
+
+
+def add_integer(a, b=98):
+    """adds a and b as integers.
+    Raises:
+        TypeError: If either of a or b is a non-integer and non-float."""
+
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    a = int(a)
+    b = int(b)
+    return a + b
