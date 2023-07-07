1515e3e1d489a21129c469e23e07c8f94176a0aa
bonheurdev <125626137+bonheurdev@users.noreply.github.com>
2023-07-08 01:39:28 +0200

    Say my name

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" define function that print the name """
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    function that print full first and last name
+
+    Args:
+        first_name
+        last_name
+    Raises:
+        TypeError: if the first name or the last name are not string
+    Returns:
+        first and last name
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    elif not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
