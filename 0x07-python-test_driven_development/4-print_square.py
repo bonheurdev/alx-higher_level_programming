1d4f78fc0b666125c0bf711110239671014b9593
bonheurdev <125626137+bonheurdev@users.noreply.github.com>
2023-07-08 02:06:34 +0200

    Print square

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Defines a function that prints square"""
+
+
+def print_square(size):
+    """Print a square.
+
+    Args:
+        size (int): The height/width of the square.
+    Raises:
+        TypeError: If size is not an integer.
+        or If size is < 0
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(size):
+        print("#" * size)
