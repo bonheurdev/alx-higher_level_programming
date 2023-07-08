fc933100acda772f21145962488c303fd52253c0
bonheurdev <125626137+bonheurdev@users.noreply.github.com>
2023-07-08 02:30:01 +0200

    Text indentation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Defines a function that indent text"""
+
+
+def text_indentation(text):
+    """Print text with two new lines after each '.', '?', and ':'.
+
+    Args:
+        text (string)
+    Raises:
+        TypeError: If text is not a string.
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    i = 0
+    while i < len(text) and text[i] == ' ':
+        i += 1
+    while i < len(text):
+        print(text[i], end="")
+        chars_symbol = ['.', '?', ':']
+        if text[i] == "\n" or text[i] in chars_symbol:
+            if text[i] in chars_symbol:
+                print("\n")
+            i += 1
+            while i < len(text) and text[i] == ' ':
+                i += 1
+            continue
+        i += 1
