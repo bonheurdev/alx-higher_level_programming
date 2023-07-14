0c2f593aef21d36a973b96160f4e4d3e9285761d
bonheurdev <125626137+bonheurdev@users.noreply.github.com>
2023-07-14 19:43:24 +0200

    check if it is sub class of a class

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""checks if sub class of a class"""
+
+
+def inherits_from(obj, a_class):
+    """Checks if an object is an inherited instance of a class.
+
+    Returns:
+        True if obj is an inherited instance of a_class
+        Otherwise - False.
+    """
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
