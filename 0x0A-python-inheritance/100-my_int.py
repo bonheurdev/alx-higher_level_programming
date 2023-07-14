2c8929511096cf2dceb748c485189e3c85a6d192
bonheurdev <125626137+bonheurdev@users.noreply.github.com>
2023-07-15 00:26:01 +0200

    invert

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""For defining a class  that inherits from int."""
+
+
+class MyInt(int):
+    """Invert int operators == and !=."""
+
+    def __eq__(self, value):
+        """invert by Overriding == opeartor with != behavior."""
+        return self.real != value
+
+    def __ne__(self, value):
+        """invert by Overriding != operator with == behavior."""
+        return self.real == value
