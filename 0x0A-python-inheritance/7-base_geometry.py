9cb5c8d10247d09a057ade5ae45bd36f65799916
bonheurdev <125626137+bonheurdev@users.noreply.github.com>
2023-07-14 20:27:53 +0200

    Integer validator

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""For defining an empty class BaseGeometry."""
+
+
+class BaseGeometry:
+    """empty base geometry class."""
+    def area(self):
+        """Non implemented function"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validate a parameter as an integer.
+
+        Args:
+            name (str).
+            value (int).
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is <= 0.
+        """
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
