69606446c4b0cc81e8632eee5d408b839519e8c2
bonheurdev <125626137+bonheurdev@users.noreply.github.com>
2023-07-15 03:24:10 +0200

    adds all arguments to a Python list, and then save them to a file

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""For defining a Python class-to-JSON function."""
+
+
+def class_to_json(obj):
+    """Return the dictionary for a simple data structure."""
+    return obj.__dict__
