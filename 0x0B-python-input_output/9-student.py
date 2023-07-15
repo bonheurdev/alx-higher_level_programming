37e71f7a087b4e5be31bdf6d83288eb4cd7167cc
bonheurdev <125626137+bonheurdev@users.noreply.github.com>
2023-07-15 03:27:44 +0200

    Student to JSON

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""For defining a class Student."""
+
+
+class Student:
+    """Represent a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new Student.
+
+        Args:
+            first_name (str)
+            last_name (str)
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """retrive a dictionary representation of the Student."""
+        return self.__dict__
