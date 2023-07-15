57f8632e22d7611d420fb0a477774e2dd1ce85ef
bonheurdev <125626137+bonheurdev@users.noreply.github.com>
2023-07-15 02:21:29 +0200

    reads a text file (UTF8) and prints it to stdout

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""For defining a text file-reading function."""
+
+
+def read_file(filename=""):
+    """reads a text file (UTF8) and prints it to stdout"""
+    with open(filename, encoding="utf-8") as fi_text:
+        print(fi_text.read(), end="")
